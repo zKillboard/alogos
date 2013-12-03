d51a55fabf0b1ce5c3e75ac90dcb3467b84bef6d
Clinton V. Weiss <cvweiss@gmail.com>
2013-12-03 19:35:43 +0000

    adding the license

--- view/index.php
+++ view/index.php
@@ -1,4 +1,20 @@
 <?php
+/* Alliance Logos
+ * Copyright (C) 2013 SquizzLabs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 $maxDate = Db::queryField("select max(logoReleased) maxDate from al_alliances", "maxDate", array(), 300);
 $latest = Db::query("select allianceID, shortName, allianceName from al_alliances where logoReleased = :maxDate order by allianceCreation", array(":maxDate" => $maxDate), 300);
